e66a36af80317aebab542ff793a79f7f3e56c59a
i4b2c <traiguuhh@gmail.com>
2023-02-01 00:32:39 +0000

    ok

--- push_swap.c
+++ push_swap.c
@@ -1,9 +1,23 @@
 #include "push_swap.h"
 
+void colocar_numero(int *num)
+{
+	int i;
+
+	i = 0;
+	while(num[i])
+	{
+		ft_printf("%d\n",num[i]);
+		i++;
+	}
+}
+
 int main(int ac, char **av)
 {
 	int *num;
 	int i;
+	int temp;
+	(void)av;
 
 	i = 0;
 	num = (int *)malloc(sizeof(int *) * ac + 1);
@@ -11,10 +25,17 @@ int main(int ac, char **av)
 	{
 		while(i+1 < ac)
 		{
-			num[i] = ft_atoi(av[i+1]);	
-			printf("%d\n",num[i]);
+			num[i] = ft_atoi(av[i+1]);
+			if(num[i] < num[i+1])
+			{
+				temp = num[i];
+				num[i] = num[i+1];
+				num[i+1] = temp;
+			}
+			ft_printf("%d\n",num[i]);
 			i++;
 		}
+		//colocar_numero(&num[0]);
 		free(num);
 	}
 	//printf("\n");
