1cfe61afd1d251c38b21d4abf2449279847bc8a3
i4b2c <117470349+i4b2c@users.noreply.github.com>
2023-02-03 04:57:49 +0000

    ok

--- push_swap.c
+++ push_swap.c
@@ -1,14 +1,18 @@
 #include "push_swap.h"
 
-void colocar_numero(int *num)
+void colocar_numero(int *num,int len)
 {
 	int i;
+	int c;
+	len--;
 
+	c = len-1;
 	i = 0;
-	while(num[i])
+	while(c >= 0)
 	{
 		ft_printf("%d\n",num[i]);
 		i++;
+		c--;
 	}
 }
 
@@ -23,14 +27,79 @@ void valor_numero(int *num, int ac, char **av)
 	}
 }
 
-void sa(int *num)
+void sa(int *stack_a)
 {
 	int temp;
 
-	temp = num[0];
-	num[0] = num[1];
-	num[1] = temp;
+	temp = stack_a[0];
+	stack_a[0] = stack_a[1];
+	stack_a[1] = temp;
 }
+
+void sb(int *stack_b)
+{
+	int temp;
+
+	temp = stack_b[0];
+	stack_b[0] = stack_b[1];
+	stack_b[1] = temp;
+}
+
+void ss(int *stack_a,int *stack_b)
+{
+	sa(stack_a);
+	sb(stack_b);
+}
+
+void ra(int *stack_a,int len)
+{
+	int temp;
+	int i;
+	len--;
+	temp = stack_a[len-1];
+	i = len-1;
+	while(i > 0)
+	{
+		stack_a[i] = stack_a[i-1];
+		i--;
+	}
+	stack_a[i] = temp;
+}
+
+
+/*
+void ra(int *stack_a,int len)
+{
+	int temp1;
+	int temp2;
+	int temp_len;
+	int i;
+	i = 0;
+	len--;
+	temp_len = stack_a[len-1];
+	while(stack_a[i] && i+1 < len)
+	{
+		if(i == 0)
+		{
+			temp1 = stack_a[i+1];
+			stack_a[i+1] = stack_a[i];
+		}
+		else if(i % 2 != 0)
+		{
+			temp2 = stack_a[i+1];
+			stack_a[i] = temp1;
+		}
+		else if(i % 2 == 0)
+		{
+			temp1 = stack_a[i+1];
+			stack_a[i] = temp2;
+		}
+		i++;
+	}
+	stack_a[0] = temp_len;
+}*/
+		
+
 
 int main(int ac, char **av)
 {
@@ -42,9 +111,8 @@ int main(int ac, char **av)
 		stack_a = (int *)malloc(sizeof(int *) * ac + 1);
 		stack_b = (int *)malloc(sizeof(int *) * ac + 1);
 		valor_numero(stack_a,ac,av);
-		if(stack_a[0] > stack_a[1])
-			sa(stack_a);
-		colocar_numero(stack_a);
+		ra(stack_a,ac);
+		colocar_numero(stack_a,ac);
 		free(stack_a);
 		free(stack_b);
 	}
