ada7b1eec915aa9804b069682d071e81a60a5f23
i4b2c <traiguuhh@gmail.com>
2023-02-04 08:12:05 +0000

    .

--- push_swap.c
+++ push_swap.c
@@ -16,11 +16,23 @@ void colocar_numero(int *num,int len)
 	}
 }
 
+void iniciar_stack_b(int *stack_b, int len)
+{
+	int i;
+
+	i = 0;
+	while(i < len)
+	{
+		stack_b[i] = 0;
+		i++;
+	}
+}
+
 void valor_numero(int *num, int ac, char **av)
 {
 	int i;
 	i = 0;
-	while(i+1 < ac)
+	while(i < ac)
 	{
 		num[i] = ft_atoi(av[i+1]);
 		i++;
@@ -51,11 +63,69 @@ void ss(int *stack_a,int *stack_b)
 	sb(stack_b);
 }
 
+void avancar(int *stack,int len)
+{
+	int i;
+	i = 0;
+	while(i < len-1)
+	{
+		stack[i] = stack[i+1];
+		i++;
+	}
+	stack[i] = 0;
+}
+
+void r_avancar(int *stack,int len)
+{
+	int i;
+	i = len-1;
+	while(i > 0)
+	{
+		stack[i] = stack[i-1];
+		i--;
+	}
+	stack[0] = 0;
+}
+
+void pa(int *stack_a,int*stack_b,int len)
+{
+	if(stack_b[0])
+	{
+		r_avancar(stack_a,len);
+		stack_a[0] = stack_b[0];
+	}
+	avancar(stack_b,len);
+}
+
+void pb(int *stack_a,int*stack_b,int len)
+{
+	if(stack_a[0])
+	{
+		r_avancar(stack_b,len);
+		stack_b[0] = stack_a[0];
+	}
+	avancar(stack_a,len);
+}
+
 void ra(int *stack_a,int len)
 {
 	int temp;
 	int i;
-	len--;
+
+	i = 0;
+	temp = stack_a[0];
+	while(i<len-1)
+	{
+		stack_a[i] = stack_a[i+1];
+		i++;
+	}
+	stack_a[i] = temp;
+}
+
+void rra(int *stack_a,int len)
+{
+	int temp;
+	int i;
 	temp = stack_a[len-1];
 	i = len-1;
 	while(i > 0)
@@ -66,55 +136,53 @@ void ra(int *stack_a,int len)
 	stack_a[i] = temp;
 }
 
-
-/*
-void ra(int *stack_a,int len)
+int organizar_numero(int *stack_a, int *stack_b, int ac)
 {
-	int temp1;
-	int temp2;
-	int temp_len;
 	int i;
+	int n;
+	(void)stack_b;
+	(void)ac;
+	n = 0;
 	i = 0;
-	len--;
-	temp_len = stack_a[len-1];
-	while(stack_a[i] && i+1 < len)
+	while(stack_a[i])
 	{
-		if(i == 0)
+		if(stack_a[0] > stack_a[1])
 		{
-			temp1 = stack_a[i+1];
-			stack_a[i+1] = stack_a[i];
-		}
-		else if(i % 2 != 0)
-		{
-			temp2 = stack_a[i+1];
-			stack_a[i] = temp1;
-		}
-		else if(i % 2 == 0)
-		{
-			temp1 = stack_a[i+1];
-			stack_a[i] = temp2;
+			sa(stack_a);
+			n++;
 		}
 		i++;
 	}
-	stack_a[0] = temp_len;
-}*/
-		
+	return (n);
+}
 
+void colocar_stacks(int *stack_a,int *stack_b, int ac)
+{
+	printf("\nStack A:\n\n");
+	colocar_numero(stack_a,ac);
+	printf("\nStack B:\n\n");
+	colocar_numero(stack_b,ac);
+	printf("\n");
+}
 
 int main(int ac, char **av)
 {
 	int *stack_a;
 	int *stack_b;
+	int len_org;
 
+	len_org = 0;
 	if(ac != 1)
 	{
 		stack_a = (int *)malloc(sizeof(int *) * ac + 1);
 		stack_b = (int *)malloc(sizeof(int *) * ac + 1);
-		valor_numero(stack_a,ac,av);
-		ra(stack_a,ac);
-		colocar_numero(stack_a,ac);
+		valor_numero(stack_a,ac-1,av);
+		iniciar_stack_b(stack_b,ac-1);
+		//len_org = organizar_numero(stack_a,stack_b,ac-1);
+		colocar_stacks(stack_a,stack_b,ac);
 		free(stack_a);
 		free(stack_b);
+		ft_printf("quantidade : %d\n",len_org);
 	}
 	return(0);
 }
--- push_swap.h
+++ push_swap.h
@@ -1,2 +1,12 @@
+#ifndef PUSH_SWAP_H
+#define PUSH_SWAP_H
 
 #include "libft/src/libft.h"
+
+typedef struct d_num
+{
+	int valor;
+	struct t_num *next;
+}	t_num;
+
+#endif
