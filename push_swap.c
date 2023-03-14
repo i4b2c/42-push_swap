e421cd693820d782a820cefc6fbe7ef204190957
i4b2c <traiguuhh@gmail.com>
2023-03-14 16:26:03 +0000

    .

--- push_swap.c
+++ push_swap.c
@@ -99,6 +99,8 @@ void dar_valor_a(t_stack **stack,char **av)
 
 int verificar_organizado(t_stack *stack)
 {
+	if(!stack)
+		return 1;
 	while(stack->next != NULL)
 	{
 		if(stack->valor >= stack->next->valor)
@@ -106,6 +108,17 @@ int verificar_organizado(t_stack *stack)
 		stack = stack->next;
 	}
 	return (0);
+}
+
+int verificar_organizado_b(t_stack *stack)
+{
+	while(stack->next != NULL)
+	{
+		if(stack->valor <= stack->next->valor)
+			return 1;
+		stack = stack->next;
+	}
+	return 0;
 }
 
 void ft_sa(t_stack **stack)
@@ -175,7 +188,6 @@ void ft_pa(t_stack **stack_a, t_stack **stack_b)
 		return;
 	adicionar_inicio(stack_b,(*stack_a)->valor);
 	remover_primeiro(stack_a);
-
 }
 
 
@@ -192,6 +204,37 @@ int organizar(t_stack **stack_a, t_stack **stack_b)
 	int i;
 
 	i = 0;
+	if(*stack_b && (*stack_b)->next)
+	{
+		if((*stack_a)->valor > (*stack_a)->next->valor
+			&& (*stack_b)->valor < (*stack_b)->next->valor)
+		{
+			ft_sa(stack_a);
+			ft_sa(stack_b);
+			ft_printf("ss\n");
+			i++;
+		}
+		else if((valor_do_ultimo(*stack_a) < (*stack_a)->valor)
+			&& valor_do_ultimo(*stack_b) > (*stack_b)->valor)
+		{
+			if((*stack_a)->valor < (*stack_a)->next->valor
+				&& (*stack_b)->valor > (*stack_b)->next->valor)
+			{
+				ft_rra(stack_a);
+				ft_rra(stack_b);
+				ft_printf("rrr\n");
+				i++;
+			}
+			else if((*stack_a)->valor > (*stack_a)->next->valor
+				&& (*stack_b)->valor < (*stack_b)->next->valor)
+			{
+				ft_ra(stack_a);
+				ft_ra(stack_b);
+				ft_printf("rr\n");
+				i++;
+			}
+		}
+	}
 	if((*stack_a)->valor > (*stack_a)->next->valor
 		&& ((valor_do_ultimo(*stack_a) > (*stack_a)->valor
 			|| valor_do_ultimo(*stack_a) < (*stack_a)->next->valor)))
@@ -201,6 +244,8 @@ int organizar(t_stack **stack_a, t_stack **stack_b)
 		i++;
 	}
 	else if((valor_do_ultimo(*stack_a) < (*stack_a)->valor))
+		//|| (valor_do_ultimo(*stack_a) > (*stack_a)->valor
+		//	&& (*stack_a)->next->valor > (*stack_a)->valor))
 	{
 		if((*stack_a)->valor < (*stack_a)->next->valor)
 		{
@@ -215,27 +260,23 @@ int organizar(t_stack **stack_a, t_stack **stack_b)
 			i++;
 		}
 	}
-	else if(verificar_organizado(*stack_a) == 1)
+	else if(!((*stack_a)->valor > (*stack_a)->next->valor)
+		&& !((valor_do_ultimo(*stack_a) < (*stack_a)->valor))
+		&& verificar_organizado(*stack_a) == 1)
 	{
 		ft_pa(stack_a,stack_b);
 		ft_printf("pb\n");
 		i++;
 	}
-	else
+	if(*stack_b)
 	{
-		ft_pa(stack_b,stack_a);
-		ft_printf("pa\n");
-		i++;
-	}
-	if(*stack_b && (*stack_b)->next)
-	{
-		if((*stack_b)->valor < (*stack_b)->next->valor)
+		if((*stack_b)->next && (*stack_b)->valor < (*stack_b)->next->valor)
 		{
 			ft_sa(stack_b);
 			ft_printf("sb\n");
 			i++;
 		}
-		else if(valor_do_ultimo(*stack_b) > (*stack_b)->valor)
+		else if((*stack_b)->next && valor_do_ultimo(*stack_b) > (*stack_b)->valor)
 		{
 			if((*stack_b)->valor > (*stack_b)->next->valor)
 			{
@@ -251,8 +292,73 @@ int organizar(t_stack **stack_a, t_stack **stack_b)
 			}
 
 		}
+		else if(verificar_organizado(*stack_a) == 0)
+		{
+			ft_pa(stack_b,stack_a);
+			ft_printf("pa\n");
+			i++;
+		}
 	}
 	return(i);
+}
+
+int organizar_3(t_stack **stack_a)
+{
+	int i;
+	i = 0;
+	if((*stack_a)->valor > (*stack_a)->next->valor
+		&& ((valor_do_ultimo(*stack_a) > (*stack_a)->valor
+			|| valor_do_ultimo(*stack_a) < (*stack_a)->next->valor)))
+	{
+		ft_sa(stack_a);
+		ft_printf("sa\n");
+		i++;
+	}
+	else if((valor_do_ultimo(*stack_a) < (*stack_a)->valor)
+		|| (valor_do_ultimo(*stack_a) > (*stack_a)->valor
+			&& (*stack_a)->next->valor > (*stack_a)->valor))
+	{
+		if((*stack_a)->valor < (*stack_a)->next->valor)
+		{
+			ft_rra(stack_a);
+			ft_printf("rra\n");
+			i++;
+		}
+		else
+		{
+			ft_ra(stack_a);
+			ft_printf("ra\n");
+			i++;
+		}
+	}
+	return i;
+}
+
+void organizar_stack_3(t_stack **stack_a)
+{
+	int i;
+	i = 0;
+	while(1)
+	{
+		if((verificar_organizado(*stack_a)) == 0)
+			break;
+		else
+			i += organizar_3(stack_a);
+	}
+	printf("organizou em %d passos\n",i);
+}
+
+void organizar_stack_5(t_stack **stack_a, t_stack **stack_b)
+{
+	int i;
+	i = 0;
+	(void)stack_a;
+	(void)stack_b;
+	while(1)
+	{
+		break;
+	}
+	printf("organizou em %d passos\n",i);
 }
 
 int verificar_stack_b(t_stack *stack)
@@ -274,7 +380,7 @@ void organizar_stacks(t_stack **stack_a, t_stack **stack_b)
 		else
 			i += organizar(stack_a,stack_b);
 	}
-	printf("organizou em %d passos\n\n",i);
+	printf("organizou em %d passos\n",i);
 }
 
 int main(int ac, char **av)
@@ -287,6 +393,11 @@ int main(int ac, char **av)
 	if(ac != 1)
 	{
 		dar_valor_a(&stack_a,av);
-		organizar_stacks(&stack_a,&stack_b);
+		if(ac == 4)
+			organizar_stack_3(&stack_a);
+		else if(ac == 5 || ac == 6)
+			organizar_stack_5(&stack_a,&stack_b);
+		else
+			organizar_stacks(&stack_a,&stack_b);
 	}
 }
